5ec12d505b56d756f15c73eb4cd8c901594f5549
Pr1yansu <113017699+Pr1yansu@users.noreply.github.com>
2024-02-27 20:19:20 +0530

    small changes in repository

--- src/main/java/com/priyansu/authentication/controllers/HistoryController.java
+++ src/main/java/com/priyansu/authentication/controllers/HistoryController.java
@@ -10,7 +10,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -24,13 +23,12 @@ public class HistoryController {
     private JwtTokenProvider jwtTokenProvider;
 
     @GetMapping("/test")
-    public ResponseEntity<String> testingHistory(){
+    public ResponseEntity<String> testingHistory() {
         return new ResponseEntity<>("History ok", HttpStatus.OK);
     }
 
     @PostMapping("/create")
-    public ResponseEntity create(@RequestBody History history, HttpServletRequest request){
-//        System.out.println(history);
+    public ResponseEntity create(@RequestBody History history, HttpServletRequest request) {
         String token = jwtTokenProvider.extractTokenFromHeader(request);
         if (token == null) {
             return new ResponseEntity<>("You are not authorized", HttpStatus.UNAUTHORIZED);
@@ -40,16 +38,16 @@ public class HistoryController {
             return new ResponseEntity<>("You are not authorized user", HttpStatus.UNAUTHORIZED);
         }
         try {
-            History Created_history = historyServices.createHistory(history,authenticatedUser);
-            return new ResponseEntity(Created_history,HttpStatus.OK);
-        }catch (Exception e){
+            History Created_history = historyServices.createHistory(history, authenticatedUser);
+            return new ResponseEntity("History is saved", HttpStatus.OK);
+        } catch (Exception e) {
             e.fillInStackTrace();
-            return new ResponseEntity("Problem to create history",HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity("Problem to create history", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @PostMapping("/view")
-    public ResponseEntity viewHistory(HttpServletRequest request){
+    public ResponseEntity viewHistory(HttpServletRequest request) {
         String token = jwtTokenProvider.extractTokenFromHeader(request);
         if (token == null) {
             return new ResponseEntity<>("You are not authorized", HttpStatus.UNAUTHORIZED);
@@ -59,28 +57,27 @@ public class HistoryController {
             return new ResponseEntity<>("You are not authorized user", HttpStatus.UNAUTHORIZED);
         }
         try {
-            List<History> L_History = new ArrayList<>();
-            L_History = historyServices.viewHistory(authenticatedUser);
-            return new ResponseEntity(L_History,HttpStatus.OK);
-        }catch (Exception e){
+            List<History> L_History = historyServices.viewHistory(authenticatedUser);
+            return new ResponseEntity(L_History, HttpStatus.OK);
+        } catch (Exception e) {
             e.fillInStackTrace();
-            return new ResponseEntity("Problem to view history list",HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity("Problem to view history list", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @PostMapping("/delete")
-    public  ResponseEntity deleteHistory(@RequestBody History history){
-        boolean status = false;
+    public ResponseEntity deleteHistory(@RequestBody History history) {
+        boolean status;
         try {
             status = historyServices.deleteHistory(history);
-            if (status){
-                return new ResponseEntity<>("History deleted successfully",HttpStatus.OK);
-            }else {
-                return new ResponseEntity<>("Error occurred to delete history",HttpStatus.INTERNAL_SERVER_ERROR);
+            if (status) {
+                return new ResponseEntity<>("History deleted successfully", HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>("Error occurred to delete history", HttpStatus.INTERNAL_SERVER_ERROR);
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             e.fillInStackTrace();
-            return new ResponseEntity<>("Problem to delete history",HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>("Problem to delete history", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
--- src/main/java/com/priyansu/authentication/service/HistoryServices.java
+++ src/main/java/com/priyansu/authentication/service/HistoryServices.java
@@ -6,41 +6,38 @@ import com.priyansu.authentication.repository.HistoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
-public class HistoryServices{
+public class HistoryServices {
     @Autowired
     private HistoryRepository historyRepository;
 
-    public History createHistory(History history, User user){
+    public History createHistory(History history, User user) {
         try {
             history.setUser(user);
             return historyRepository.save(history);
-        }catch (Exception e){
+        } catch (Exception e) {
             e.fillInStackTrace();
             return null;
         }
     }
 
-    public List<History> viewHistory(User user){
+    public List<History> viewHistory(User user) {
         try {
-            List<History> L_history = new ArrayList<>();
-            L_history = historyRepository.findByUserId(user.getId());
-            return L_history;
-        }catch(Exception e){
+            return historyRepository.findAllByUserId(user.getId());
+        } catch (Exception e) {
             e.fillInStackTrace();
             return null;
         }
     }
 
-    public boolean deleteHistory(History history){
+    public boolean deleteHistory(History history) {
         boolean status = false;
         try {
             historyRepository.deleteById(history.getH_id());
             status = true;
-        }catch (Exception e){
+        } catch (Exception e) {
             e.fillInStackTrace();
         }
         return status;
